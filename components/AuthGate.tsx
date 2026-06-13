933a766ec9916c24e3a5023e2c4939ac6daf101f
GG-369 <guillermo.gamboa@utec.edu.pe>
2026-06-12 23:40:34 -0500

    Update Auth routes

--- components/AuthGate.tsx
+++ components/AuthGate.tsx
@@ -22,7 +22,7 @@ export default function AuthGate() {
       : { username, email, password };
 
     try {
-      const res = await fetch(`http://127.0.0.1:8000${endpoint}`, {
+      const res = await fetch(`https://buda-backend.onrender.com${endpoint}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
